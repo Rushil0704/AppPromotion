ec8cbbf0def03e0ed080a5b7dab83a1ba944f1f9
Khush-dbc <khush.dbc@gmail.com>
2025-12-07 15:32:05 +0530

    general changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Header from './component/Header'
 import Home from './Pages/Home'
 
--- src/component/Home/Hero.tsx
+++ src/component/Home/Hero.tsx
@@ -1,6 +1,3 @@
-import React from 'react'
-
-
 const Hero = () => {
 	return (
 		<div className="relative w-full bg-white overflow-hidden">
